2afd25876abc1a7424b2d6fbc49f2a30695a7b9d
Fahrudin Hariadi <fahrudin.hariadi@gmail.com>
2025-08-02 03:44:08 +0000

    fix : env baru

--- env.sh
+++ env.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
 find ./ -type f -name ".env" -exec rm {} \;
-find ./config -type f -name ".env" -exec rm {} \;
 find ./config -type f -name "db_backup.cnf" -exec rm {} \;
 find ./config -type f -name "init-backup-user.sql" -exec rm {} \;
-cp ./scripts/sample.env ./config/.env
+cp ./env.sample ./.env
 cp ./scripts/sample.cnf ./config/db_backup.cnf
 cp ./scripts/sample.sql ./config/init-backup-user.sql
 
@@ -13,10 +12,10 @@ export DBAPPUSERX=$(curl --silent https://randomuser.me/api/ | jq '.results[].lo
 export DBAPPPASSWORDX=$(curl --silent https://randomuser.me/api/ | jq '.results[].login.salt')
 export PASSYBACKUPX=$(curl --silent https://randomuser.me/api/ | jq '.results[].login.salt')
 
-sed -i~ "/^DB_ROOTPASSWORD=/s/=.*/=$DBROOTPASSWORDX/" ./config/.env
-sed -i~ "/^DB_APPUSER=/s/=.*/=$DBAPPUSERX/" ./config/.env
-sed -i~ "/^DB_APPPASS=/s/=.*/=$DBAPPPASSWORDX/" ./config/.env
-sed -i~ "/^BACKUP_PASS=/s/=.*/=$PASSYBACKUPX/" ./config/.env
+sed -i~ "/^DB_ROOTPASSWORD=/s/=.*/=$DBROOTPASSWORDX/" ./.env
+sed -i~ "/^DB_APPUSER=/s/=.*/=$DBAPPUSERX/" ./.env
+sed -i~ "/^DB_APPPASS=/s/=.*/=$DBAPPPASSWORDX/" ./.env
+sed -i~ "/^BACKUP_PASS=/s/=.*/=$PASSYBACKUPX/" ./.env
 
 if [[ "$OSTYPE" == "darwin"* ]]; then
     SED_INPLACE="sed -i ''"
@@ -24,15 +23,13 @@ else
     SED_INPLACE="sed -i"
 fi
 
-eval "$SED_INPLACE 's/\"//gI' ./config/.env"
+eval "$SED_INPLACE 's/\"//gI' ./.env"
 
-source ./config/.env
+source ./.env
 
 eval "$SED_INPLACE \"s/BACKUP_PASS/$PASSYBACKUPX/gI\" ./config/init-backup-user.sql"
 eval "$SED_INPLACE \"s/PASSYBACKUPX/$PASSYBACKUPX/gI\" ./config/db_backup.cnf"
 eval "$SED_INPLACE 's/\"//gI' ./config/init-backup-user.sql"
 eval "$SED_INPLACE 's/\"//gI' ./config/db_backup.cnf"
 
-rm ./config/.env~
-
-ln config/.env .env
+rm ./.env~
